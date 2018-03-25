f212babdc7e0ee970b7a265b5796811c27e309a4
phonebone <mistermacbook90@gmail.com>
2018-03-25 11:18:15 +0200

    change map styles

--- src/Map.js
+++ src/Map.js
@@ -71,6 +71,215 @@ export class Map extends Component {
 			// create a new Google Maps map and put it in the DOM element with ref 'map'
 			map = new google.maps.Map(this.map, {
 				center: {lat: 52.0914165, lng: 5.1147089},
+				styles: [
+					{
+						"featureType": "administrative",
+						"elementType": "labels.text.fill",
+						"stylers": [
+							{
+								"color": "#444444"
+							}
+						]
+					},
+					{
+						"featureType": "landscape",
+						"elementType": "all",
+						"stylers": [
+							{
+								"color": "#f2f2f2"
+							}
+						]
+					},
+					{
+						"featureType": "poi",
+						"elementType": "all",
+						"stylers": [
+							{
+								"visibility": "off"
+							}
+						]
+					},
+					{
+						"featureType": "road",
+						"elementType": "all",
+						"stylers": [
+							{
+								"saturation": -100
+							},
+							{
+								"lightness": 45
+							}
+						]
+					},
+					{
+						"featureType": "road.highway",
+						"elementType": "all",
+						"stylers": [
+							{
+								"visibility": "simplified"
+							}
+						]
+					},
+					{
+						"featureType": "road.highway",
+						"elementType": "geometry",
+						"stylers": [
+							{
+								"visibility": "simplified"
+							},
+							{
+								"color": "#ff6a6a"
+							},
+							{
+								"lightness": "0"
+							}
+						]
+					},
+					{
+						"featureType": "road.highway",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#ee3123"
+							}
+						]
+					},
+					{
+						"featureType": "road.highway",
+						"elementType": "geometry.stroke",
+						"stylers": [
+							{
+								"color": "#ee3123"
+							}
+						]
+					},
+					{
+						"featureType": "road.highway",
+						"elementType": "labels.text",
+						"stylers": [
+							{
+								"visibility": "on"
+							}
+						]
+					},
+					{
+						"featureType": "road.highway",
+						"elementType": "labels.icon",
+						"stylers": [
+							{
+								"visibility": "on"
+							}
+						]
+					},
+					{
+						"featureType": "road.arterial",
+						"elementType": "all",
+						"stylers": [
+							{
+								"visibility": "on"
+							}
+						]
+					},
+					{
+						"featureType": "road.arterial",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"color": "#ee3123"
+							},
+							{
+								"lightness": "62"
+							}
+						]
+					},
+					{
+						"featureType": "road.arterial",
+						"elementType": "labels.icon",
+						"stylers": [
+							{
+								"visibility": "off"
+							}
+						]
+					},
+					{
+						"featureType": "road.local",
+						"elementType": "geometry.fill",
+						"stylers": [
+							{
+								"lightness": "75"
+							}
+						]
+					},
+					{
+						"featureType": "transit",
+						"elementType": "all",
+						"stylers": [
+							{
+								"visibility": "off"
+							}
+						]
+					},
+					{
+						"featureType": "transit.line",
+						"elementType": "all",
+						"stylers": [
+							{
+								"visibility": "on"
+							}
+						]
+					},
+					{
+						"featureType": "transit.station.bus",
+						"elementType": "all",
+						"stylers": [
+							{
+								"visibility": "on"
+							}
+						]
+					},
+					{
+						"featureType": "transit.station.rail",
+						"elementType": "all",
+						"stylers": [
+							{
+								"visibility": "on"
+							}
+						]
+					},
+					{
+						"featureType": "transit.station.rail",
+						"elementType": "labels.icon",
+						"stylers": [
+							{
+								"weight": "0.01"
+							},
+							{
+								"hue": "#ff0028"
+							},
+							{
+								"lightness": "0"
+							}
+						]
+					},
+					{
+						"featureType": "water",
+						"elementType": "all",
+						"stylers": [
+							{
+								"visibility": "on"
+							},
+							{
+								"color": "#80e4d8"
+							},
+							{
+								"lightness": "25"
+							},
+							{
+								"saturation": "-23"
+							}
+						]
+					}
+				],
 				zoom: 16
 			});
 
